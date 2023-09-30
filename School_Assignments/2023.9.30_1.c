b18ed1f9c929d091db8baa4c1fa68fde16bc4409
ABKpazu <145771707+ABKpazu@users.noreply.github.com>
2023-09-30 22:09:14 +0800

    Create 2023.9.30_1.c

---
+++ School_Assignments/2023.9.30_1.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    double a, b, c;
+    double l, m, n;
+    double R_cos, R_sin, R_S;
+    printf("Enter the length of three sides:\n");
+    scanf("%lf %lf %lf", &a, &b, &c);
+
+    if (b > a)
+    {
+        if(c > b)
+        {
+            l = c, m = a, n = b;
+        }
+        else
+        {
+            l = b, m = c, n = a;
+        }
+    }
+    else if (c > a)
+    {
+        l = c, m = a, n = b;
+    }
+    else
+    {
+        l = a, m = b, n = c;
+    }
+    if (m + n <= l || a <= 0 || b <= 0 || c <= 0)
+    {
+        printf("Unreasonable length!\n");
+        return 0;
+    }
+    R_cos = (pow(m, 2) + pow(n, 2) - pow(l, 2)) / (2 * m * n);
+    R_sin = sqrt(1 - pow(R_cos, 2));
+    R_S = 0.5 * m * n * R_sin;
+
+    printf("The sine value of the largest angle is %f\nThe value of area is %f\n", R_sin, R_S);
+    return 0;
+}
