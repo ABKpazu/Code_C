81f5eb395e28528b8fd8192a3856fe4fcf38d9f7
ABKpazu <145771707+ABKpazu@users.noreply.github.com>
2023-10-19 12:17:40 +0800

    Add files via upload

---
+++ School_Assignments/2023.10.18_6.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <math.h>
+
+int isPrime(int n);
+
+int main()
+{
+    int n, m;
+    int num = 0;
+    int i;
+    printf("Please enter a positive integer:\n");
+    scanf("%d", &n);
+
+    if (isPrime(n))
+    {
+        printf("This number is a prime number.\n");
+    }
+    else
+    {
+        printf("This number is NOT a prime number.\n");
+    }
+
+    printf("Please enter values for n and m:\n");
+    scanf("%d %d", &n, &m);
+
+    for (i = n; i <=m; i++)
+    {
+        if (isPrime(i))
+        {
+            printf("%d\n", i);
+            num++;
+        }
+    }
+    printf("The number of prime numbers is: %d\n", num);
+    return 0;
+}
+
+int isPrime(int n)
+{
+    int i;
+    if (n == 1)
+    {
+        return 0;
+    }
+    for (i = 2; i <= sqrt(n); i++)
+    {
+        if (!(n % i))
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
